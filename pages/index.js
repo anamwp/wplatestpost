8743d0d441565f0e49d7108fdb2e96f810bde892
Md. Anam Hossain <anam@themexpert.com>
2021-02-25 12:25:19 +0600

    initial commint

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -20,33 +21,10 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <Link href="/wpmayor"><a> WP Mayor </a></Link>
+            <Link href="/wptavern"><a> WP Tavern </a></Link>
+            <Link href="/torquemag"><a> Torque Mag </a></Link>
+            <Link href="/gutenbergtimes"><a> Gutenberg Times </a></Link>
         </div>
       </main>
 
---
+++ pages/torquemag.jsx
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from 'react'
+import Moment from 'react-moment';
+import Link from 'next/link'
+
+export default function WPMayor() {
+    const [posts, setPosts] = useState({
+        loading: false, 
+        data: []
+    });
+
+    useEffect(() => {
+        setPosts({loading: true});
+        const api = 'https://torquemag.io/wp-json/wp/v2/posts';
+        fetch(api)
+        .then(res => res.json())
+        .then(data => {
+            setPosts({loading: false, data: data});
+        });
+    }, [])
+
+    return (
+        <div>
+            <Link href="/"><a> Back </a></Link>
+            <h2>Torque Mag</h2>
+
+            {
+                posts.loading 
+                ? 
+                'Loading'
+                : 
+                posts.data.map(post => {
+                    return(
+                        <div key={post.id}>
+                            <h2>
+                                <a href={post.link} target="_blank">
+                                {`${post.title.rendered}`}
+                                </a>
+                            </h2>
+                            <Moment date={post.date} />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
