aa3d63fe9b4c9520aa844bc0ed0dae4a8e734ad3
Md. Anam Hossain <anam@themexpert.com>
2021-02-25 12:39:53 +0600

    export command added and style added in front page

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,43 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import styled from 'styled-components'
+const LinksWrapper = styled.div`
+  display:grid;
+  grid-template-columns:repeat(4, 1fr);
+  a{
+    border:solid 1px #ddd;
+    padding:5px 15px;
+    background:#f7f7f7;
+    text-align:center;
+    display:inline-block;
+    margin:5px;
+  }
+`
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>WordPress Latest Post</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          WordPress Top Blog Sites
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Check Latest post by clicking on individual Sites
         </p>
 
-        <div className={styles.grid}>
+        <LinksWrapper>
             <Link href="/wpmayor"><a> WP Mayor </a></Link>
             <Link href="/wptavern"><a> WP Tavern </a></Link>
             <Link href="/torquemag"><a> Torque Mag </a></Link>
             <Link href="/gutenbergtimes"><a> Gutenberg Times </a></Link>
-        </div>
+        </LinksWrapper>
       </main>
 
       <footer className={styles.footer}>
